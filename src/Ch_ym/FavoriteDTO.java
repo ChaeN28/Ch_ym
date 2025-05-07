700881ea6e5f5bb2501a2e412bf224c48916fdd3
Ch27s <dbcotmd0@gmail.com>
2025-05-07 16:29:40 +0900

    개인플젝 commit

---
+++ src/Ch_ym/FavoriteDTO.java
@@ -0,0 +1,7 @@
+package Ch_ym;
+
+public class FavoriteDTO {
+	int favorite_no;
+	int mem_no;
+	int music_no;
+}
---
+++ src/Ch_ym/MusicDTO.java
@@ -0,0 +1,17 @@
+package Ch_ym;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor	
+@NoArgsConstructor
+public class MusicDTO {
+
+	private int music_no;
+	private String music_title;
+	private String music_singer;
+}
